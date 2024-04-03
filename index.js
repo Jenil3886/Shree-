0f9f1c7da74d3b37bc8e400a020471bfbdac4ddb
jenil gajera <gajerajenil08@gmail.com>
2024-04-03 21:41:45 +0530

    changed folder structure

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// view rawstyle.css hosted with  by GitHub Here is the complete index.js file Source Code
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 
 const toggleNavbar = () => {
-    // alert("Plz Subscribe ");
-    nav_header.classList.toggle("active");
+  nav_header.classList.toggle("active");
 }
 
-    ;
-
 mobile_nav.addEventListener("click", () => toggleNavbar());
-/* view rawindex.js hosted with by GitHub */
-
-// Hero section 
 
 let slideIndex = 0;
 showSlides();
@@ -27,17 +20,18 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+setTimeout(showSlides, 2000); // Change image every 2 seconds
 
 // product card
 
 document.querySelectorAll('.add-to-cart').forEach(button => {
   button.addEventListener('click', () => {
-      alert('Added to cart!');
+    alert('Added to cart!');
   });
 });
