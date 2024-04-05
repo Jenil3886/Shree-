6a91846fff5374dd070fb51afd72e155ae875d68
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-04-05 22:19:22 +0530

    dropdown issue fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,298 +1,247 @@
 const newPanelData = [
-    {
-        title: 'Men',
-        items: [
-            'Shoes',
-            'Accessories',
-            'Apparels'
-        ],
-    },
-    {
-        title: 'Women',
-        items: [
-            'Shoes',
-            'Bags',
-            'Accessories',
-            'Apparels'
-        ],
-    },
-    {
-        title: 'Kids',
-        items: [
-            'Shoes',
-            'Accessories'
-        ],
-    },
+  {
+    title: 'Men',
+    items: ['Shoes', 'Accessories', 'Apparels'],
+  },
+  {
+    title: 'Women',
+    items: ['Shoes', 'Bags', 'Accessories', 'Apparels'],
+  },
+  {
+    title: 'Kids',
+    items: ['Shoes', 'Accessories'],
+  },
 ]
 
 const menPanelData = [
-    {
-        title: 'Shoes',
-        items: [
-            'Casual Shoes',
-            'Chappal & Flipflop',
-            'Formal Shoes',
-            'Loafers & Moccassin',
-            'Sandals & Floater',
-            'Sports'
-        ],
-    },
-    {
-        title: 'Apparel',
-        items: [
-            'Joggers & Bottoms',
-            'Shorts',
-            'T-Shirt & Polos'
-        ],
-    },
-    {
-        title: 'Collections',
-        items: [
-            'Online Exclusives',
-            'Festive & Ethnic',
-            'Party-Bling',
-            '365 Closet',
-            'Workwear',
-            'Lifestyle'
-        ],
-    },
-    {
-        title: 'Brands',
-        items: [
-            'Puma',
-            'Nike',
-            'Adidas',
-            'Abross',
-            'Campus',
-            'Bata',
-            'Sparks',
-            'Liberty',
-            'Indus',
-            'TRV',
-            'Runner'
-        ],
-    },
+  {
+    title: 'Shoes',
+    items: [
+      'Casual Shoes',
+      'Chappal & Flipflop',
+      'Formal Shoes',
+      'Loafers & Moccassin',
+      'Sandals & Floater',
+      'Sports',
+    ],
+  },
+  {
+    title: 'Apparel',
+    items: ['Joggers & Bottoms', 'Shorts', 'T-Shirt & Polos'],
+  },
+  {
+    title: 'Collections',
+    items: [
+      'Online Exclusives',
+      'Festive & Ethnic',
+      'Party-Bling',
+      '365 Closet',
+      'Workwear',
+      'Lifestyle',
+    ],
+  },
+  {
+    title: 'Brands',
+    items: [
+      'Puma',
+      'Nike',
+      'Adidas',
+      'Abross',
+      'Campus',
+      'Bata',
+      'Sparks',
+      'Liberty',
+      'Indus',
+      'TRV',
+      'Runner',
+    ],
+  },
 ]
 
 const womenPanelData = [
-    {
-        title: 'Shoes',
-        items: [
-            'Ballerina',
-            'Casual Shoes',
-            'Chappal',
-            'Flipflop',
-            'Sandals',
-            'Sports'
-        ],
-    },
-    {
-        title: 'Heel Type',
-        items: [
-            'Block',
-            'Cone',
-            'Kitten',
-            'Platform',
-            'Stilettos',
-            'Wedges'
-        ],
-    },
-    {
-        title: 'Collections',
-        items: [
-            'Online Exclusives',
-            'Festive & Ethnic',
-            'Party-Bling',
-            '365 Closet',
-            'Workwear',
-            'Lifestyle'
-        ],
-    },
-    {
-        title: 'Brands',
-        items: [
-            'Puma',
-            'Nike',
-            'Adidas',
-            'Mochi',
-            'Inc.5',
-            'Bata',
-            'Paragon',
-            'Liberty',
-            'Catwalk',
-            'Clarks',
-            'Indus'
-        ],
-    },
+  {
+    title: 'Shoes',
+    items: [
+      'Ballerina',
+      'Casual Shoes',
+      'Chappal',
+      'Flipflop',
+      'Sandals',
+      'Sports',
+    ],
+  },
+  {
+    title: 'Heel Type',
+    items: ['Block', 'Cone', 'Kitten', 'Platform', 'Stilettos', 'Wedges'],
+  },
+  {
+    title: 'Collections',
+    items: [
+      'Online Exclusives',
+      'Festive & Ethnic',
+      'Party-Bling',
+      '365 Closet',
+      'Workwear',
+      'Lifestyle',
+    ],
+  },
+  {
+    title: 'Brands',
+    items: [
+      'Puma',
+      'Nike',
+      'Adidas',
+      'Mochi',
+      'Inc.5',
+      'Bata',
+      'Paragon',
+      'Liberty',
+      'Catwalk',
+      'Clarks',
+      'Indus',
+    ],
+  },
 ]
 
 const kidsPanelData = [
-    {
-        title: 'Featured',
-        items: [
-            'New Arrivals',
-            'Best Seller'
-
-        ],
-    },
-    {
-        title: 'Shoes',
-        items: [
-            'Boys',
-            'Girls',
-            'Infant',
-            'School'
-        ],
-    },
-    {
-        title: 'Brands',
-        items: [
-            'Nike',
-            'Puma',
-            'Crocs',
-            'Adidas',
-            'Abross',
-            'Campus',
-            'Bata',
-            'Sparks',
-            'Liberty'
-        ],
-    },
+  {
+    title: 'Featured',
+    items: ['New Arrivals', 'Best Seller'],
+  },
+  {
+    title: 'Shoes',
+    items: ['Boys', 'Girls', 'Infant', 'School'],
+  },
+  {
+    title: 'Brands',
+    items: [
+      'Nike',
+      'Puma',
+      'Crocs',
+      'Adidas',
+      'Abross',
+      'Campus',
+      'Bata',
+      'Sparks',
+      'Liberty',
+    ],
+  },
 ]
 
 const salePanelData = [
-    {
-        title: 'Men',
-        items: [
-            'Shoes',
-            'Accessories',
-            'Apparels'
-        ],
-    },
-    {
-        title: 'Women',
-        items: [
-            'Shoes',
-            'Bags',
-            'Accessories',
-            'Apparels'
-        ],
-    },
-    {
-        title: 'Kids',
-        items: [
-            'Shoes',
-            'Accessories'
-        ],
-    },
+  {
+    title: 'Men',
+    items: ['Shoes', 'Accessories', 'Apparels'],
+  },
+  {
+    title: 'Women',
+    items: ['Shoes', 'Bags', 'Accessories', 'Apparels'],
+  },
+  {
+    title: 'Kids',
+    items: ['Shoes', 'Accessories'],
+  },
 ]
 
 const brandsPanelData = [
-    {
-        title: 'Casual Shoes',
-        items: [
-            'Nike',
-            'Puma',
-            'Adidas',
-            'Liberty',
-            'Fila',
-            'Reebok',
-            'crocs',
-            'Skechers',
-        ]
-    },
-    {
-        title: 'Formal Shoes',
-        items: [
-            'Bata',
-            'Mochi',
-            'Clarks',
-            'timberland',
-            'Woodland',
-        ]
-    },
-    {
-        title: 'Sport Shoes',
-        items:[
-            'Nike',
-            'Puma',
-            'Adidas',
-            'Sparx',
-            'Campas',
-            'Abros',
-            'Runner'
-        ]
-    }
-    
+  {
+    title: 'Casual Shoes',
+    items: [
+      'Nike',
+      'Puma',
+      'Adidas',
+      'Liberty',
+      'Fila',
+      'Reebok',
+      'crocs',
+      'Skechers',
+    ],
+  },
+  {
+    title: 'Formal Shoes',
+    items: ['Bata', 'Mochi', 'Clarks', 'timberland', 'Woodland'],
+  },
+  {
+    title: 'Sport Shoes',
+    items: ['Nike', 'Puma', 'Adidas', 'Sparx', 'Campas', 'Abros', 'Runner'],
+  },
 ]
 
 function showDropdown(elem) {
-    switch (elem.id) {
-        case 'new-dropdown':
-            showNewDropdownPanel(newPanelData)
-            break
+  switch (elem.id) {
+    case 'new-dropdown':
+      showNewDropdownPanel(newPanelData)
+      break
 
-        case 'men-dropdown':
-            showNewDropdownPanel(menPanelData)
-            break
+    case 'men-dropdown':
+      showNewDropdownPanel(menPanelData)
+      break
 
-        case 'women-dropdown':
-            showNewDropdownPanel(womenPanelData)
-            break
+    case 'women-dropdown':
+      showNewDropdownPanel(womenPanelData)
+      break
 
-        case 'kids-dropdown':
-            showNewDropdownPanel(kidsPanelData)
-            break
+    case 'kids-dropdown':
+      showNewDropdownPanel(kidsPanelData)
+      break
 
-        case 'sale-dropdown':
-            showNewDropdownPanel(salePanelData)
-            break
+    case 'sale-dropdown':
+      showNewDropdownPanel(salePanelData)
+      break
 
-        case 'brands-dropdown':
-            showNewDropdownPanel(brandsPanelData)
-            break
-    }
-}
-
-function hideDropdown() {
-    const dropdown = document.querySelector('.open-dropdown')
-
-    while (dropdown.firstChild) {
-        dropdown.removeChild(dropdown.firstChild)
-    }
-
-    dropdown.style.display = 'none'
+    case 'brands-dropdown':
+      showNewDropdownPanel(brandsPanelData)
+      break
+  }
 }
 
 function showNewDropdownPanel(panelData) {
-    const dropdown = document.querySelector('.open-dropdown')
+  const dropdown = document.querySelector('.open-dropdown')
 
-    for (let column of panelData) {
-        const dropdownList = document.createElement('div')
-        dropdownList.classList.add('dropdown-list')
+  while (dropdown.firstChild) {
+    dropdown.removeChild(dropdown.firstChild)
+  }
 
-        const dropdownListTitle = document.createElement('h3')
-        dropdownListTitle.classList.add('dropdown-list-title')
+  for (let column of panelData) {
+    const dropdownList = document.createElement('div')
+    dropdownList.classList.add('dropdown-list')
 
-        const dropdownListItems = document.createElement('div')
-        dropdownListItems.classList.add('dropdown-list-items')
+    const dropdownListTitle = document.createElement('h3')
+    dropdownListTitle.classList.add('dropdown-list-title')
 
-        dropdownListTitle.textContent = column.title
+    const dropdownListItems = document.createElement('div')
+    dropdownListItems.classList.add('dropdown-list-items')
 
-        for (let item of column.items) {
-            const dropdownListItem = document.createElement('span')
-            dropdownListItem.classList.add('dropdown-list-item')
+    dropdownListTitle.textContent = column.title
 
-            dropdownListItem.textContent = item
+    for (let item of column.items) {
+      const dropdownListItem = document.createElement('span')
+      dropdownListItem.classList.add('dropdown-list-item')
 
-            dropdownListItems.appendChild(dropdownListItem)
-        }
+      dropdownListItem.textContent = item
 
-        dropdownList.appendChild(dropdownListTitle)
-        dropdownList.appendChild(dropdownListItems)
-
-        dropdown.appendChild(dropdownList)
+      dropdownListItems.appendChild(dropdownListItem)
     }
 
-    dropdown.style.display = 'flex'
+    dropdownList.appendChild(dropdownListTitle)
+    dropdownList.appendChild(dropdownListItems)
+
+    dropdown.appendChild(dropdownList)
+  }
+
+  dropdown.style.display = 'flex'
+  dropdown.style.padding = '3rem 0'
+}
+
+function visibleDropdown() {
+  const dropdown = document.querySelector('.open-dropdown')
+
+  dropdown.style.display = 'flex'
+  dropdown.style.padding = '3rem 0'
+}
+
+function hideDropdown() {
+  const dropdown = document.querySelector('.open-dropdown')
+
+  dropdown.style.display = 'none'
 }
